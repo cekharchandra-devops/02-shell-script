f3bd634a388bccc3ccc8c2f2ecda68921c57fe5f
cekharchandra-devops <cekharchandra.devops@gmail.com>
2024-12-14 10:22:54 +0530

    committed

---
+++ 05-vars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter username:"
+read USERNAME
+
+echo "username entered: $USERNAME"
+
+echo "please enter password:"
+read -s PASSWORD
+
+echo "password entered: $PASSWORD"
