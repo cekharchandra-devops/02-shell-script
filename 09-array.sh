0313be6672aaf44edb5bc9c9d1a6ea04889e71d1
cekharchandra-devops <cekharchandra.devops@gmail.com>
2024-12-16 15:51:41 +0530

    committed

--- 09-array.sh
+++ 09-array.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-names=("chandra","sekhar","reddy")
+# names=("chandra","sekhar","reddy") it will be considered as one string. use space b/w elements instead of comma
+names=("chandra" "sekhar" "reddy")
 
-echo "first index ${names[0]}"
-echo "second index ${names[1]}"
-echo "all elements ${names[@]}"
+echo "first index: ${names[0]}"
+echo "second index: ${names[1]}"
+echo "all elements: ${names[@]}"
