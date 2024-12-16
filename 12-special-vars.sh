49a750bfa3d4bc25dab4c811f7c2b13bd05b7e88
cekharchandra-devops <cekharchandra.devops@gmail.com>
2024-12-16 16:30:37 +0530

    committed

--- 12-special-vars.sh
+++ 12-special-vars.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 echo "args list passed to the script: ${@}"
+echo "args list passed to the script: $*"
 echo "number of args: ${#}"
 echo "script name: ${0}"
 echo "pwd: ${PWD}"
 echo "home directory of the user ${HOME}"
 eval last_arg=\$$#
 echo "first, second and last args passed ot the script ${0} ${1} ${last_arg}"
+echo "PID of the script $$"
+ls /non-existing-directory
+echo "exit status of previous command $?"
+
+echo "hell0"
+echo "last arg passed to previous command $_
+sleep 100 &
+echo "process id of last backgournd task $!"
